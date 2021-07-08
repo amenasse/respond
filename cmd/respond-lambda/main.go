146da25cb5f28ca85905faa009fab3f85c04e306
Anthony Menasse <amenasse@gmail.com>
2021-07-08 15:25:23 +1000

    Lambda handler can be set with environment var

--- cmd/respond-lambda/main.go
+++ cmd/respond-lambda/main.go
@@ -4,10 +4,25 @@ import (
 	"github.com/amenasse/respond/aws/lambda"
 	"github.com/amenasse/respond/cmd"
 	runtime "github.com/aws/aws-lambda-go/lambda"
+        "log"
+        "os"
 )
 
 func main() {
 
 	code := cmd.GetStatusCode()
-	runtime.Start(lambda.ApiGatewayV2Handler(code))
+        handler_name := "api-v2"
+        if env_var := os.Getenv("RESPONSE_LAMBDA_HANDLER"); env_var != "" {
+            handler_name = env_var
+        }
+
+	if handler_name == "api-v2" {
+		runtime.Start(lambda.ApiGatewayV2Handler(code))
+	} else if handler_name == "api-v1" {
+
+		runtime.Start(lambda.ApiGatewayV1Handler(code))
+	} else
+        {
+            log.Fatalf("Unknown handler: %s",handler_name)
+    }
 }
